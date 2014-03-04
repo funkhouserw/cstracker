e00079072446f258576cc95984570890e3ee57a5
TJ <thmsjcbs@gmail.com>
2014-03-03 21:50:55 -0800

    Fix respond_to in stats

--- app/models/stats.rb
+++ app/models/stats.rb
@@ -60,4 +60,8 @@ class Stats
   def method_missing(sym, *args, &block)
     data[sym.to_s] || super(sym, *args, &block)
   end
+
+  def respond_to?(sym, include_private = false)
+    data.try(:[], sym.to_s) != nil || super(sym, include_private)
+  end
 end
