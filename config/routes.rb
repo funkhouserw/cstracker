29ab3f4ee53eca668b0ab8c785abdd4b5e7db33c
TJ <thmsjcbs@gmail.com>
2014-03-05 19:11:06 -0800

    Add basic style examples

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 SteamStatTracker::Application.routes.draw do
-  resources :players, :only => [:index, :show] do
+  resources :players, :only => [:show] do
     collection do
       get 'search'
     end
   end
 
-  root to: "players#index"
+  root to: "welcome#index"
 end
