014e26d24c520fd39c40797c85bd56e1212d8c5f
tjacobs3 <thmsjcbs@gmail.com>
2014-05-02 11:17:21 -0500

    Move app id to constant

--- app/services/steam_api_service.rb
+++ app/services/steam_api_service.rb
@@ -1,12 +1,13 @@
 class SteamApiService
   attr_reader :player
+  CSGO_APP_ID = 730
 
   def initialize(_player)
     @player = _player
   end
 
   def download_player_stats
-    uri = URI("http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=#{app_id}&key=#{api_key}&steamid=#{player.steam_id}")
+    uri = URI("http://api.steampowered.com/ISteamUserStats/GetUserStatsForGame/v0002/?appid=#{CSGO_APP_ID}&key=#{api_key}&steamid=#{player.steam_id}")
     raw_stats = JSON.parse(Net::HTTP.get(uri))["playerstats"]["stats"]
     Stats.create(player_id: player.id, data: parse_stats(raw_stats))
   end
@@ -14,10 +15,6 @@ class SteamApiService
   private
   def api_key
     Rails.configuration.steam["api_key"]
-  end
-
-  def app_id
-    730 #CSGO
   end
 
   def parse_stats(data_hash)
