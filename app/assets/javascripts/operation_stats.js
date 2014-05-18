549fee7fdec8ef768e1f5a5d7f61e77c9f049036
tjacobs3 <thmsjcbs@gmail.com>
2014-05-18 12:53:02 -0500

    Add style to operation stats

--- app/assets/javascripts/operation_stats.js
+++ app/assets/javascripts/operation_stats.js
@@ -3,19 +3,38 @@ var operationStats = {
 
   initialize: function (data) {
     this.operation_data = data;
-    var that = this;
-    $.each(this.operation_data, function(id, op_data) {
-      that.addOperationData(op_data);
-    });
+    this.addOperationData(data);
   },
   
   addOperationData: function(_data) {
-    var $op_content = $("<div>", {class: "operation_content"});
+    $operation_skeleton = $("#operation_stat_skeleton");
+    var operation_dom_elements = [];
+    var that = this;
 
-    $.each(_data, function(attr_name, value) {
-      $op_content.append("<div>" + attr_name + ": " + value + "</div>");
+    $.each(_data, function( index, operation ) {
+      var $operation_element = $operation_skeleton.clone();
+      $operation_element.removeAttr('id');
+      $operation_element.css("display", "");
+      $operation_element.find(".operation_title").text(operation["name"]);
+      $operation_element.find("table").append(that.statToTableRowString("Competitive Match Wins", operation["comp_wins"]));
+      $operation_element.find("table").append(that.statToTableRowString("Competitive Kills", operation["comp_kills"]));
+      $operation_element.find("table").append(that.statToTableRowString("3k Rounds", operation["comp_3k"]));
+      $operation_element.find("table").append(that.statToTableRowString("4k Rounds", operation["comp_4k"]));
+      $operation_element.find("table").append(that.statToTableRowString("5k Rounds", operation["comp_5k"]));
+      $operation_element.find("table").append(that.statToTableRowString("Headshot Percentage", operation["comp_hs"].toFixed(2) + "%"));
+      $operation_element.find("table").append(that.statToTableRowString("Competitive MVPs", operation["comp_mvp"]));
+
+      operation_dom_elements.push($operation_element);
+
+      if((index + 1) % 3 == 0) {
+        if(index > 0 ) operation_dom_elements.push($("<br class='clear' />"));
+      }
     });
 
-    $("#operation_stats_container").prepend($op_content);
+    $("#operation_stats_container").append(operation_dom_elements);
+  },
+
+  statToTableRowString: function(name, value) {
+    return "<tr><td class=\"stat_title\">" + name + "</td><td class=\"stat stat_accent\">" + value + "</td></tr>";
   }
 }
